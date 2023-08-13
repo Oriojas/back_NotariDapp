59f38b0277f4ed167d9f9d6769914d88ca0a5743
oscar50 <oscar@whaleandjaguar.co>
2023-08-13 09:12:57 -0500

    feat: create environment deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,34 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('@openzeppelin/hardhat-upgrades');
+
+const dotenv = require('dotenv');
+dotenv.config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.9",
+  networks: {
+    // for optimisim
+    "optimisc-goerli": {
+      url: 'https://goerli.optimism.io/',
+      accounts: [process.env.PRIVATE_KEY],
+      gasPrice: 1000000000,
+      chainId: 420
+    },
+    // for base
+    "base-goerli": {
+      url: "https://goerli.base.org",
+      accounts: [process.env.PRIVATE_KEY],
+      gasPrice: 1000000000,
+      chainId: 84531
+    },
+    // for zora
+    "zora-goerli": {
+      url: "https://testnet.rpc.zora.energy/",
+      accounts: [process.env.PRIVATE_KEY],
+      gasPrice: 1000000000,
+      chainId: 999
+    },
+  },
 };
+
